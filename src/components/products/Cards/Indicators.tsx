14e0e43dcfa584fe92dcb0443b8945f4a458a219
myrevers <myrevers@hotmail.com>
2022-02-13 01:05:07 +0100

    remove comment

--- src/components/products/Cards/Indicators.tsx
+++ src/components/products/Cards/Indicators.tsx
@@ -50,36 +50,3 @@ export default function Indicators() {
         </div>
     );
 }
-
-{/* <div className="space-y-6 w-full">
-            <h1 className="text-3xl font-medium">Dashboard</h1>
-            <div className="flex items-center space-x-6 w-full">
-            <div className="space-y-2 rounded-md bg-[#001926] p-4 flex-1">
-                <div className="flex items-center space-x-2 text-xs font-bold uppercase text-app-primary">
-                <span>Market Cap</span>
-                </div>
-                <div className="text-2xl">${marketCap}</div>
-            </div>
-            <div className="space-y-2 rounded-md bg-[#001926] p-4 flex-1">
-                <div className="flex items-center space-x-2 text-xs font-bold uppercase text-app-primary">
-                <span>Liquidity Pool</span>
-                </div>
-                <div className="text-2xl">${liquidityPool}</div>
-            </div>
-            <div className="space-y-2 rounded-md bg-[#001926] p-4 flex-1">
-                <div className="flex items-center space-x-2 text-xs font-bold uppercase text-app-primary">
-                <span>Holders</span>
-                </div>
-                <div className="text-2xl">{holders}</div>
-            </div>
-            <div className="space-y-2 rounded-md bg-[#001926] p-4 flex-1">
-                <div className="flex items-center space-x-2 text-xs font-bold uppercase text-app-primary">
-                <span>Price</span>
-                </div>
-                <div className="text-2xl">${price}</div>
-            </div>
-            </div>
-            <NoSsr>
-                <TVChartContainer />
-            </NoSsr>
-        </div> */}
