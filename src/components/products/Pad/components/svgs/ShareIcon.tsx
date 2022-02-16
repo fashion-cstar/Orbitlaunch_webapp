4f5d4ba06a914b7d84836b93bc1e518571203df7
fashion-cstar <phillipiao89123@gmail.com>
2022-02-17 03:18:15 +0900

    delete svg assets and add svg components

---
+++ src/components/products/Pad/components/svgs/ShareIcon.tsx
@@ -0,0 +1,11 @@
+import createSvg from "../../../../svgs/createSvg";
+
+export default createSvg((props) => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="6" cy="12" r="3" stroke="#919699" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <circle cx="18" cy="6" r="3" stroke="#919699" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <circle cx="18" cy="18" r="3" stroke="#919699" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M8.69922 10.7008L15.2992 7.30078" stroke="#919699" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M8.69922 13.3008L15.2992 16.7008" stroke="#919699" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+));
