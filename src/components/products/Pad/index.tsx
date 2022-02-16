f650e9773954afc424397cf8d4c6fb4de6e39d40
fashion-cstar <phillipiao89123@gmail.com>
2022-02-17 03:20:24 +0900

    add feat featured and ended projects table

--- src/components/products/Pad/index.tsx
+++ src/components/products/Pad/index.tsx
@@ -1,10 +1,13 @@
-import React, { useMemo, useState, useEffect } from 'react'
-import BuyButton from "@app/components/common/BuyButton"
-import UpcomingCard from "./components/UpcomingCard"
+import React, { useMemo, useState, useEffect, useRef  } from 'react'
+import BuyButton from "./components/Buttons/BuyButton"
+import FeaturedList from './components/FeaturedList'
 import moment from 'moment'
 import { IDO_LIST } from "./constants/Idos"
-import RightArrow from './components/RightArrow'
-import LeftArrow from './components/LeftArrow'
+import RightArrow from './components/Buttons/RightArrow'
+import LeftArrow from './components/Buttons/LeftArrow'
+import EndedTabHeader from './components/EndedTable/EndedTabHeader'
+import EndedIdoTable from './components/EndedTable'
+import { useRouter } from 'next/router'
 
 const styleHeight = {
     height: '300px',
@@ -13,48 +16,73 @@ const styleHeight = {
 }
 
 export default function Pad() {
-    const IdoListComplete = IDO_LIST // fetch this list from the server
-    const IdoListFiltered = useMemo(() => {        
+    const [filterChain, setChainId] = useState(0)
+    const [IdoEndedFiltered, setIdoEndedFiltered]=useState<any>()
+    const [tableWidth, setTableWidth]=useState(1)
+    const widthRef = useRef<any>();
+    const router = useRouter()
+    const IdoListComplete = IDO_LIST // fetch this list from the server    
+    const IdoUpcomingFiltered = useMemo(() => {        
         return IdoListComplete.filter(item => moment(item?.launchDate ?? '').isAfter(moment.now()))
-    }, [IdoListComplete])
+    }, [IdoListComplete])    
+    
     const [cardIndex, setCardIndex] = useState(0)    
     const handleLeftClick=()=>{
         if (cardIndex>0) setCardIndex(cardIndex-1)
-        else setCardIndex(IdoListFiltered.length-1)                  
+        else setCardIndex(IdoUpcomingFiltered.length-1)                  
     }
     const handleRightClick=()=>{
-        if (cardIndex<IdoListFiltered.length-1) setCardIndex(cardIndex+1)
+        if (cardIndex<IdoUpcomingFiltered.length-1) setCardIndex(cardIndex+1)
         else setCardIndex(0)
     }
+    const handleTabClick = (id:number) => {
+        setChainId(id)
+    }    
+    const onDetail = (ido:any) => {
+        router.push({
+            pathname: '/pad',
+            query: { project: ido.project },
+          })
+    }
+    useEffect(() => {        
+        setIdoEndedFiltered(IdoListComplete.filter(item => moment(item?.endDate ?? '').isBefore(moment.now())))        
+    }, [IdoListComplete])
 
+    useEffect(()=>{
+        if (filterChain===0) setIdoEndedFiltered(IdoListComplete.filter(item => moment(item?.endDate ?? '').isBefore(moment.now())))
+        else setIdoEndedFiltered(IdoListComplete.filter(item => moment(item?.endDate ?? '').isBefore(moment.now()) && item.chainId===filterChain))
+    }, [filterChain])
+
+    const getListSize = () => {   
+        if (widthRef){
+            const newWidth = widthRef?.current?.clientWidth;
+            setTableWidth(newWidth)          
+        }
+    };
+
+    useEffect(() => {
+        const newWidth = widthRef?.current?.clientWidth;
+        setTableWidth(newWidth)         
+        window.addEventListener("resize", getListSize);
+    }, []);
     return (
-        <>
-        <div className="flex flex-col space-y-4 w-full">
-            <div className="flex flex-row items-center">
+        <div className="w-full" ref={widthRef}>
+            <div className="flex flex-row items-center justify-between">
                 <h1 className="text-[40px] font-medium">OrbitPad</h1>
-                <BuyButton></BuyButton>
+                <BuyButton />
             </div>
             <div className="mt-8">
-                <h1 className="text-white text-[24px]">See upcoming projects</h1>
-                <div className='overflow-hidden' style={{right:"-50px"}}>
-                    <div className="mt-4 block whitespace-nowrap overflow-visible relative w-1">
-                        {IdoListFiltered.map((item, index) => {
-                            return (
-                                <UpcomingCard status="Sold out" symbol={item.tokenSymbol} project={item.project} 
-                                    chainId={item.chainId} totalRaise={item.totalRaise} allocationMin={item.allocationMin} 
-                                    allocationMax={item.allocationMax} hero={item.hero} firstCardIndex={cardIndex} />
-                            )
-                        })}                    
+                <h1 className="text-white text-[24px]">Featured projects</h1>
+                <FeaturedList IdoUpcomingFiltered={IdoUpcomingFiltered} cardIndex={cardIndex} onDetail={onDetail} />     
+                    <div className='flex justify-end mt-4 w-full mr-8'>
+                        <div className="flex flex-row gap-8">
+                            <LeftArrow handleLeftClick={handleLeftClick} />
+                            <RightArrow handleRightClick={handleRightClick} />
+                        </div>
                     </div>
-                </div>
-                <div className='flex justify-end mt-4 w-full mr-8'>
-                    <div className="flex flex-row gap-8">
-                        <LeftArrow handleLeftClick={handleLeftClick} />
-                        <RightArrow handleRightClick={handleRightClick} />
-                    </div>
-                </div>
             </div>
+            <EndedTabHeader handleTabClick={handleTabClick} />
+            {IdoEndedFiltered && <EndedIdoTable idos={IdoEndedFiltered} width={tableWidth} />}
         </div>
-        </>
     )
 }
