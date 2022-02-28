8456485f9d2366201341e8f8f5b1395f14d3ad26
fashion-cstar <phillipiao89123@gmail.com>
2022-02-28 23:39:32 +0800

    update branch

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack5: false,
+  webpack5: false,  
+  env: {
+    email_service: {
+      service_id: 'service_dcua6vi',
+      template_id: 'template_51h5coh',
+      user_id: 'user_3wmEsh0wKIP6I8OSMqdBO',
+    },
+    ReCAPTCHA_key: '6LfKFI8eAAAAAERwV3epJMyO-t8wm9afBrZrtEWL'
+  },
 };
