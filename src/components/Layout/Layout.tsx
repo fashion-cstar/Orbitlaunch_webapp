5629cb54e8b8b53bf0a1a31aea94b9a00aff1d69
myrevers <myrevers@hotmail.com>
2022-03-03 23:31:14 +0100

    (common): add message for mobile version temporary

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@mui/material";
+import { useRouter } from "next/router";
 import React, { PropsWithChildren } from "react";
 import BottomNav from "./BottomNav";
 import Header from "./Header";
@@ -8,6 +10,13 @@ const gradientColor = {
 }
 
 export default function Layout({ children }: PropsWithChildren<{}>) {
+
+  const router = useRouter();
+
+  const goToPad = () => {
+    router.push({ pathname: '/pad' })
+  };
+
   return (
     <>
       <Header />
@@ -15,9 +24,29 @@ export default function Layout({ children }: PropsWithChildren<{}>) {
         <div className="tempdesktop">
           <Sidebar isOpen />
         </div>
-        <div className="p-10 grow">
-          {children}
-        </div>
+        {(router.pathname === '/pad') ? (
+          <div className="p-10 grow">
+            {children}
+          </div>
+        ) : (
+          <div className="p-10 grow text-center">
+            We're working to release the responsive versions of the Fund/Pad/Dashboard over the next few days.<br /><br />
+            To access this immediately please visit on a laptop/desktop device.<br /><br />
+
+            <br /><br />
+            Good news!<br />
+            OrbitPad is already compatible with mobile.<br /><br />
+
+            <Button
+              variant="outlined"
+              onClick={goToPad}
+              className="relative"
+              sx={{ borderRadius: "12px" }}
+            >
+              Navigate to OrbitPad
+            </Button>
+          </div>
+        )}
         <div className="tempmobile fixed bottom-0 w-full h-[45px] border-t border-t-[#112b40]" style={gradientColor}>
           <BottomNav />
         </div>
