e1991fd3ffa11dc1343ed6d1e6968b256a27f523
myrevers <myrevers@hotmail.com>
2022-03-03 03:05:20 +0100

    navigate on board from logo

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
       <div className="fixed top-0 left-0 flex min-h-[64px] w-full items-center border-b border-b-[#112b40]" style={gradientColor}>
         <div className="flex w-full items-center justify-between px-6">
           <div className="flex items-center space-x-2">
-            <LogoHorizontal className="h-[16px]" />
+            <a href="/"><LogoHorizontal className="h-[16px]" /></a>
           </div>
 
           <div className="tempmobile">
