01017484459fa87560eb6e1410047478fb7f9e5d
fashion-cstar <phillipiao89123@gmail.com>
2022-03-06 15:11:34 +0800

    resolved css warning

--- src/components/products/Pad/components/Common/InputBoxContainer.tsx
+++ src/components/products/Pad/components/Common/InputBoxContainer.tsx
@@ -1,26 +1,11 @@
 import { ReactNode } from 'react';
-import styled from 'styled-components'
-
-const Container = styled.div`
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin-top: 0px;
-    }
-
-    /* Firefox */
-    input[type=number] {
-    -moz-appearance: textfield;
-    }
-
-    display: flex;
-    align-items: center;
-`
 
 export default function InputBoxContainer({ children }: { children: ReactNode }) {
     return (
-        <Container>
-            {children}
-        </Container>
+        <div>
+            <form autoComplete="off">
+                {children}
+            </form>
+        </div>
     )
 }
