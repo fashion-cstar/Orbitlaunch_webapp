74b4f54f9741228b1253fdfd4ce4101c8a483867
fashion-cstar <phillipiao89123@gmail.com>
2022-03-14 03:00:35 +0800

    update busd balance after deposit

--- src/components/products/Pad/components/ProjectDetail/JoinPresaleModal.tsx
+++ src/components/products/Pad/components/ProjectDetail/JoinPresaleModal.tsx
@@ -13,7 +13,8 @@ import {
     useNativeTokenBalance,
     useTokenAllowance,
     useTotalInvestedAmount,
-    useInvestCap
+    useInvestCap,
+    useTokenBalanceCallback
 } from 'src/state/Pad/hooks'
 import { AddressZero } from '@ethersproject/constants'
 import CircularProgress from '@mui/material/CircularProgress';
@@ -47,8 +48,9 @@ export default function JoinPresaleModal({ isOpen, launchTokenPrice, currentTier
     const [userMaxAllocation, setUserMaxAllocation] = useState(0)
     const userDepositedAmount = useDepositInfo(project.contractAddress, project.blockchain)
     const tokenDecimals = uselaunchTokenDecimals(project.contractAddress, project.blockchain)
-    const userDepositToken = useToken(BUSDTokenAddress[chainId], project.blockchain)
-    const userBUSDBalance = useTokenBalance(BUSDTokenAddress[chainId], project.blockchain)
+    const userDepositToken = useToken(BUSDTokenAddress[chainId], project.blockchain)    
+    const { tokenBalanceCallback } = useTokenBalanceCallback() 
+    const [userBUSDBalance, setUserBUSDBalance] = useState<BigNumber>(BigNumber.from(0))
     const nativeBalance = useNativeTokenBalance(project.blockchain)
     const [ethBalance, setEthBalance] = useState(0)
     const [fundDecimals, setFundDecimals] = useState(18)
@@ -64,6 +66,16 @@ export default function JoinPresaleModal({ isOpen, launchTokenPrice, currentTier
             setEthBalance(formatEther(nativeBalance, 18, 5))
         }
     }, [nativeBalance])
+
+    useEffect(() => {
+        if (isOpen){
+            if (account){
+                callUserBUSDCallback()
+            }else{
+                setUserBUSDBalance(BigNumber.from(0))
+            }
+        }
+    }, [account, isOpen])
 
     useEffect(() => {
         if (userDepositedAmount) {
@@ -89,12 +101,23 @@ export default function JoinPresaleModal({ isOpen, launchTokenPrice, currentTier
         }
     }, [tokenDecimals])
 
+    const callUserBUSDCallback = () =>{
+        try {
+            tokenBalanceCallback(BUSDTokenAddress[chainId], project.blockchain).then((res: BigNumber) => {
+                setUserBUSDBalance(res)
+            }).catch((error: any) => {
+                console.log(error)
+            })
+        } catch (error) {                
+            console.debug('Failed to get user BUSD balance', error)
+        } 
+    }
     async function onApprove() {
         setIsWalletApproving(true)
         try {
-            padApproveCallback(project.contractAddress, BUSDTokenAddress[chainId], fundTokenAmount, project.blockchain).then((hash: string) => {
+            padApproveCallback(project.contractAddress, BUSDTokenAddress[chainId], Math.round(fundTokenAmount), project.blockchain).then((hash: string) => {
                 setIsApproved(true)
-                setIsWalletApproving(false)
+                setIsWalletApproving(false)                
             }).catch((error: any) => {
                 setIsWalletApproving(false)
                 console.log(error)
@@ -108,6 +131,7 @@ export default function JoinPresaleModal({ isOpen, launchTokenPrice, currentTier
 
     const successDeposited = () => {
         setDepositedAmount(depositedAmount.add(parseEther(fundTokenAmount, fundDecimals)))
+        callUserBUSDCallback()
     }
 
     async function onDeposit() {
