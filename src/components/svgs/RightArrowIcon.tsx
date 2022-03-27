8763f289b04fc80c7ebef17747c02fc03478b2a3
fashion-cstar <phillipiao89123@gmail.com>
2022-03-28 07:44:10 +0800

    fix import path

--- src/components/svgs/RightArrowIcon.tsx
+++ src/components/svgs/RightArrowIcon.tsx
@@ -1,4 +1,4 @@
-import createSvg from "../../../../svgs/createSvg";
+import createSvg from "./createSvg";
 
 export default createSvg((props) => (
     <svg width="28" height="28" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
