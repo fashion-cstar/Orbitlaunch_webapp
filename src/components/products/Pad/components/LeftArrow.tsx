349a58aebe66d2f03f8a8c80700ff79737ef690a
fashion-cstar <phillipiao89123@gmail.com>
2022-03-28 07:24:05 +0800

    delete old LeftArrow, RightArrow from Pad

--- src/components/products/Pad/components/LeftArrow.tsx
+++
@@ -1,7 +0,0 @@
-export default function LeftArrow({ handleLeftClick }: { handleLeftClick: () => void }) {
-    return (
-        <div className="cursor-pointer" onClick={handleLeftClick}>
-            <img className="rotate-180 w-6" src="/images/launchpad/svg/RightArrow.svg" />
-        </div>
-    )
-}
