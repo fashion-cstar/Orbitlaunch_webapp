5d8e196387d329fba8cd7a4adbfc686e8630d998
fashion-cstar <phillipiao89123@gmail.com>
2022-03-28 07:27:20 +0800

    fix: exchange SelectBox part with CommonSelectBox

--- src/components/products/Pad/components/Common/SelectBox.tsx
+++ src/components/products/Pad/components/Common/SelectBox.tsx
@@ -1,5 +1,13 @@
-export default function SelectBox({ name, value, source, onChange }:
-    { name: string, value: any, source: any, onChange: (val: any) => void }) {
+import CommonSelectBox from 'src/components/common/CommonSelectBox'
+
+interface SelectBoxProps {
+    name: string
+    value: any
+    source: any
+    onChange: (val: any) => void 
+}
+
+export default function SelectBox({ name, value, source, onChange }: SelectBoxProps) {
 
     return (
         <div className="flex-1 rounded-2xl bg-[#001926] py-4 px-6">
@@ -7,13 +15,7 @@ export default function SelectBox({ name, value, source, onChange }:
             <div className="flex items-center space-x-3 text-[12px] font-bold uppercase text-app-primary mb-2">
                 <span>{name}</span><span className='text-[#ff0000]'>*</span>
             </div>
-            <select className='bg-[#001926] text-white text-[16px] rounded-md block w-full p-0 focus:outline-none'>
-                {
-                    source.map((item) => {
-                        return <option value={item.value} selected={item.value === value ? true : false}>{item.label}</option>
-                    })
-                }
-            </select>
+            <CommonSelectBox value={value} source={source} onChange={onChange} />            
         </div>
     )
 }
