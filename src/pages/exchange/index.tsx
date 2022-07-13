a0c10a4970b86f7923034a3b75701262bab10778
fashion-cstar <phillipiao89123@gmail.com>
2022-07-13 22:44:40 +0800

    update exchange

--- src/pages/exchange/index.tsx
+++ src/pages/exchange/index.tsx
@@ -1,14 +1,43 @@
 import Head from "next/head";
-import Exchange from "@app/components/products/Exchange";
+import Exchange from "@app/components/products/Exchange/index";
+import { GetServerSideProps } from "next";
+import { getBNBPrice } from "@app/lib/hooks/useTokenPrice";
+import {
+  GlobalCurrencyProvider,
+  GlobalCurrencyState,
+} from "@app/lib/context/GlobalCurrencyContext";
 
-export default function Home() {
+interface PageWithGlobalCurrencyProps extends GlobalCurrencyState { }
+
+export default function ExchangePage(props: PageWithGlobalCurrencyProps) {
   return (
-    <>
+    <GlobalCurrencyProvider bnbPrice={props.bnbPrice}>
       <Head>
         <title>OrbitExchange</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Exchange></Exchange>
-    </>
+    </GlobalCurrencyProvider>
   );
 }
+
+// export default function ExchangePage() {
+//   return (
+//     <>
+//       <Head>
+//         <title>OrbitExchange</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+//       <Exchange></Exchange>
+//     </>
+//   );
+// }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const bnbPrice = await getBNBPrice();
+  return {
+    props: {
+      bnbPrice,
+    },
+  };
+};
