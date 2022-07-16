5ac7064f3f27cb531077779cb8aee19b382d7534
fashion-cstar <phillipiao89123@gmail.com>
2022-07-16 21:36:58 +0800

    update swap tokens list

--- src/shared/SwapTokens.ts
+++ src/shared/SwapTokens.ts
@@ -1,11 +1,11 @@
 const tokens = [
     {
-        "name": "WBNB Token",
-        "symbol": "WBNB",
+        "name": "BNB Token",
+        "symbol": "BNB",
         "address": "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c.png"
+        "logoURI": "./images/tokens/bnb.png"
     },
     {
         "name": "WolfEye",
@@ -21,7 +21,7 @@ const tokens = [
         "address": "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0xe9e7cea3dedca5984780bafc599bd69add087d56.png"
+        "logoURI": "./images/tokens/busd.png"
     },
     {
         "name": "Ethereum Token",
@@ -29,7 +29,7 @@ const tokens = [
         "address": "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0x2170ed0880ac9a755fd29b2688956bd959f933f8.png"
+        "logoURI": "./images/tokens/eth.png"
     },
     {
         "name": "BTCB Token",
@@ -37,7 +37,7 @@ const tokens = [
         "address": "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c.png"
+        "logoURI": "./images/tokens/btcb.png"
     },
     {
         "name": "Tether USD",
@@ -45,7 +45,7 @@ const tokens = [
         "address": "0x55d398326f99059fF775485246999027B3197955",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0x55d398326f99059ff775485246999027b3197955.png"
+        "logoURI": "./images/tokens/usdt.png"
     },
     {
         "name": "PancakeSwap Token",
@@ -53,7 +53,7 @@ const tokens = [
         "address": "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82.png"
+        "logoURI": "./images/tokens/cake.png"
     },
     {
         "name": "Venus",
@@ -61,7 +61,7 @@ const tokens = [
         "address": "0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0xcf6bb5389c92bdda8a3747ddb454cb7a64626c63.png"
+        "logoURI": "./images/tokens/xvs.png"
     },
     {
         "name": "VAI Stablecoin",
@@ -69,7 +69,7 @@ const tokens = [
         "address": "0x4BD17003473389A42DAF6a0a729f6Fdb328BbBd7",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0x4bd17003473389a42daf6a0a729f6fdb328bbbd7.png"
+        "logoURI": "./images/tokens/vai.png"
     },
     {
         "name": "Pancake Bunny",
@@ -77,7 +77,7 @@ const tokens = [
         "address": "0xC9849E6fdB743d08fAeE3E34dd2D1bc69EA11a51",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0xc9849e6fdb743d08faee3e34dd2d1bc69ea11a51.png"
+        "logoURI": "./images/tokens/bunny.png"
     },    
     {
         "name": "Alpaca",
@@ -85,7 +85,7 @@ const tokens = [
         "address": "0x8F0528cE5eF7B51152A59745bEfDD91D97091d2F",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0x8f0528ce5ef7b51152a59745befdd91d97091d2f.png"
+        "logoURI": "./images/tokens/alpaca.png"
     },
     {
         "name": "Belt",
@@ -93,7 +93,7 @@ const tokens = [
         "address": "0xE0e514c71282b6f4e823703a39374Cf58dc3eA4f",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0xE0e514c71282b6f4e823703a39374Cf58dc3eA4f.png"
+        "logoURI": "./images/tokens/belt.png"
     },
     {
         "name": "TokoCrypto",
@@ -101,7 +101,7 @@ const tokens = [
         "address": "0x9f589e3eabe42ebC94A44727b3f3531C0c877809",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0x9f589e3eabe42ebc94a44727b3f3531c0c877809.png"
+        "logoURI": "./images/tokens/tko.png"
     },
     {
         "name": "Nerve Finance",
@@ -109,7 +109,7 @@ const tokens = [
         "address": "0x42F6f551ae042cBe50C739158b4f0CAC0Edb9096",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0x42f6f551ae042cbe50c739158b4f0cac0edb9096.png"
+        "logoURI": "./images/tokens/nrv.png"
     },
     {
         "name": "Ellipsis",
@@ -117,7 +117,7 @@ const tokens = [
         "address": "0xA7f552078dcC247C2684336020c03648500C6d9F",
         "chainId": 56,
         "decimals": 18,
-        "logoURI": "./images/tokens/0xa7f552078dcc247c2684336020c03648500c6d9f.png"
+        "logoURI": "./images/tokens/eps.png"
     }
 ]
 
