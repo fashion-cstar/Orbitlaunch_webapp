f402aea9eb91be9eb83b4c1638772c67458429a3
fashion-cstar <phillipiao89123@gmail.com>
2022-07-18 16:46:57 +0800

    update play contract address

--- src/shared/PadConstant.ts
+++ src/shared/PadConstant.ts
@@ -29,5 +29,5 @@ export const RpcProviders: { [chainId in ChainId]?: JsonRpcProvider } = {
     [ChainId.Rinkeby]: new ethers.providers.JsonRpcProvider('https://rinkeby.infura.io/v3/b6a2f439eeb57f2c3c4334a6/eth/rinkeby'),
     [ChainId.BSC]: new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org'),    
     // [ChainId.BSCTestnet]: new ethers.providers.JsonRpcProvider('https://data-seed-prebsc-1-s1.binance.org:8545')
-    [ChainId.BSCTestnet]: new ethers.providers.JsonRpcProvider('https://bold-aged-morning.bsc-testnet.discover.quiknode.pro/82655c5444c700267b843dcc8f3dad0f3df0b6c4')
+    [ChainId.BSCTestnet]: new ethers.providers.JsonRpcProvider('https://frosty-green-meadow.bsc-testnet.discover.quiknode.pro/730cb84479b7eab3f63bcd1906ae12d5d33cac13')
 }
--- src/shared/PlayConstant.ts
+++ src/shared/PlayConstant.ts
@@ -7,6 +7,6 @@ export const Roshambo_MaxBet = 1250;
 export const SpinWheel_MinBet = 250;
 export const SpinWheel_MaxBet = 1250;
 
-export const OrbitPlayContractAddress = "0xe72d337630c72Bce21902E2D04fA116Fd9f488F6";
+export const OrbitPlayContractAddress = "0xDDE93d31e00868294E86bEaC415E965ad871b50F";
 //just for test
 export const OrbtTokenAddress = "0xD3C7eeC9daA9b061303CFe54F880978D7D033987"
